e4ccfbe0811b4d4d0d935696576c4407dec7c53d
mikolaj <bulmikolaj@interia.pl>
2022-01-15 20:40:42 +0100

    added router

--- script.sh
+++ script.sh
@@ -1,3 +1,8 @@
+sudo docker run --init -p 7447:7447/tcp -p 8000:8000/tcp eclipse/zenoh &
+curl -X PUT -H 'content-type:application/properties' -d 'path_expr=/bank/**' http://localhost:8000/@/router/local/plugin/storages/backend/memory/storage/my-storage &
+
+sleep 2
+
 python3 manager.py &
 
 sleep 2
